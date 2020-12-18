c9a6681805ff1dcf858dc211b5813094183afed6
2ByungJun <ieed0205@naver.com>
2020-12-18 09:52:41 +0900

    #1 뷰 게시판 프로젝트

---
+++ components/BoardList.vue
@@ -0,0 +1,30 @@
+<template>
+  <p>{{msg}}</p>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    data (){
+        return {
+          msg: 'hi'
+        }
+    },
+    created(){
+      this.init()
+    },
+    methods : {
+      init(){
+        axios.post('api/hello.json').then(res => {
+          this.msg = res.data
+          console.log(res)
+        })
+      }
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ views/Board.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="board">
+    <BoardList />
+  </div>
+</template>
+
+<script>
+import BoardList from '../components/BoardList.vue'
+
+export default {
+  components:{
+    BoardList,
+  }
+}
+</script>
